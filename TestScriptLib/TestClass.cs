e474ffcd4ffb77d4fcff28182db6ec78d09c74f2
tom.zhang <tom.zhang@optoauto.com>
2019-03-02 19:18:54 +0800

    加入一个库，让脚本可任一调用

---
+++ TestScriptLib/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScriptLib
+{
+    public class TestClass
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
